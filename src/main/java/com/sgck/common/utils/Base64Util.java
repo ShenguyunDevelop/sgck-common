d2641bcb460cd8e603ca7b5391ab376fcf4a01a7
ysantac <xyyy2008xyyy@163.com>
2017-05-09 18:51:11 +0800

    添加MD5Util

--- src/main/java/com/sgck/common/utils/Base64Util.java
+++ src/main/java/com/sgck/common/utils/Base64Util.java
@@ -1,5 +1,10 @@
 package com.sgck.common.utils;
 
+import java.io.UnsupportedEncodingException;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
 public class Base64Util {
 
 	public static final String   BASE64_SG_CHARS   =  "fckD3V74jteRWu2+5i6slzLmFTYASygIOvh0KBX/rEJnwNbaG1pCqUZQ8M9dPoxH=";
@@ -17,4 +22,35 @@ public class Base64Util {
 		}
 		return re;
 	}
+	
+	// 加密  
+    public static String getBase64(String str) {  
+        byte[] b = null;  
+        String s = null;  
+        try {  
+            b = str.getBytes("utf-8");  
+        } catch (UnsupportedEncodingException e) {  
+            e.printStackTrace();  
+        }  
+        if (b != null) {  
+            s = new BASE64Encoder().encode(b);  
+        }  
+        return s;  
+    }  
+  
+    // 解密  
+    public static String getFromBase64(String s) {  
+        byte[] b = null;  
+        String result = null;  
+        if (s != null) {  
+            BASE64Decoder decoder = new BASE64Decoder();  
+            try {  
+                b = decoder.decodeBuffer(s);  
+                result = new String(b, "utf-8");  
+            } catch (Exception e) {  
+                e.printStackTrace();  
+            }  
+        }  
+        return result;  
+    }  
 }
